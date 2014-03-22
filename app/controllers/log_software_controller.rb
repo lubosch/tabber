b1ea1ab6b9df350c33a82ad6a716e28780ef25ae
= <=>
2014-03-22 21:28:14 +0100

    fixnute vela veci

--- app/controllers/log_software_controller.rb
+++ app/controllers/log_software_controller.rb
@@ -4,11 +4,12 @@ class LogSoftwareController < ActionController::Base
   def create
     @user = User.find(params[:user_id])
     if @user 
+      @user.update_ip(request.remote_ip)
       if !params[:process_name].to_s.blank?
 
-      s = Software.find_or_create_by_process(:process => params[:process_name], :filepath => params[:filepath], :name => params[:description])
-      ls = LogSoftware.last
-      ls = LogSoftware.create(:softwareWindowName => params[:window_name], :timestamp => DateTime.now, :user => @user, :software => s) if ls.softwareWindowName != params[:window_name]
+      s = Software.find_or_create_by_process(:process => params[:process_name], :filepath => params[:filepath], :name => params[:description], :ignore => 0)
+      ls = @user.last_log
+      ls = LogSoftware.create(:softwareWindowName => params[:window_name], :timestamp => DateTime.now, :user => @user, :software => s) if !ls || ls.softwareWindowName != params[:window_name]
 end    
 render json: ls
 else
--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -3,7 +3,7 @@ class SongController < ApplicationController
   include ApplicationHelper
 
   def create
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:user_id]) if (params[:user_id])
     if @user
       s = Software.find_by_process(params[:software_name])
       @song = Song.find_or_create_by_name_and_artist(:fileName => params[:filename], :name => params[:name], :artist => params[:artist], :length => params[:length], :genre => params[:genre])
