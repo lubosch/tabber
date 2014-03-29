4c1954a9d41f39793868f9da177a4907252fe4d4
lubosch <lubomir.vnenk@zoho.com>
2014-03-29 07:08:02 +0100

    activity shower

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,14 +1,14 @@
 class ActivitiesController < ApplicationController
   def show
 
-    #user = User.find(6)
-    #if (params[:range])
-    #  date = Time.parse(params[:range])
-    #  @logs = user.last_log_software_tabber(date)
-    #  #binding.pry
-    #else
-    #  @logs = user.last_log_software_tabber(24.hours.ago)
-    #end
+    user = User.find(6)
+    if (params[:range])
+      date = Time.parse(params[:range])
+      @logs = user.last_log_software_tabber(date)
+      #binding.pry
+    else
+      @logs = user.last_log_software_tabber(24.hours.ago)
+    end
 
 
     respond_to do |format|
---
+++ app/models/skriptik.rb
@@ -0,0 +1,15 @@
+class Skriptik
+  def fix_encoding
+    LogSoftware.find(:all).each do |ls|
+      begin
+        encoded = ls.softwareWindowName.encode("windows-1250", :undefined => :replace, :replace => "").force_encoding('utf-8')
+        if encoded.valid_encoding? && encoded != ls.softwareWindowName
+          ls.softwareWindowName = encoded
+          ls.save
+        end
+      rescue Exception => ex
+        binding.pry
+      end
+    end
+  end
+end
