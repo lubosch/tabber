49edebec7c97616ff33e6258ed85d47b31068538
= <=>
2014-03-29 08:42:48 +0100

    da

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,13 +1,6 @@
 class ActivitiesController < ApplicationController
   def show
 
-<<<<<<< HEAD
-    user = current_user
-    if (params[:range])
-      date = Time.parse(params[:range])
-      @logs = user.last_log_software_tabber(date)
-      #binding.pry
-=======
     #user = User.find(6)
 
     user = current_user
@@ -20,7 +13,6 @@ class ActivitiesController < ApplicationController
       else
         @logs = user.last_log_software_tabber(24.hours.ago)
       end
->>>>>>> 5946e7d03528a6cd273480908316105fed7218cd
     else
 
       render 'static_pages/not_authorized'
