b176e8fafca1789bb7b6e0f9b0dd8c757bb9ff69
= <=>
2014-04-14 07:01:49 +0200

    ok

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,7 +7,7 @@ class Video < ActiveRecord::Base
 
   def log(software, user)
     length = 7200 if !length || length == 0    
-    last_lm = LogMovie.where('',:software_id => software, :movie_id => self, :user_id => user).where("started > ?", length.seconds.ago).first
+    last_lm = LogMovie.where(:software_id => software, :movie_id => self, :user_id => user).where("started > ?", length.seconds.ago).first
     if !last_lm
       LogMovie.create(:software => software, :user => user, :movie => self, :started => DateTime.now)
     end
